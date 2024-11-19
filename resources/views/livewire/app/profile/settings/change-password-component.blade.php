a3691a554f6acd2a75e895bb358f3100ca07adaf
ahmedsujon <sujonahmed424@gmail.com>
2024-11-19 22:36:06 +0600

    password change

--- resources/views/livewire/app/profile/settings/change-password-component.blade.php
+++ resources/views/livewire/app/profile/settings/change-password-component.blade.php
@@ -1,6 +1,6 @@
 <div>
     <section class="forget_password_wrapper">
-        <form action="" class="form_area h-full-screen space-between horizontal-m-w pt-12">
+        <form wire:submit.prevent='updatePassword' class="form_area h-full-screen space-between horizontal-m-w pt-12">
             <div class="w-100">
                 <div class="back_btn_grid">
                     <button type="button" class="page_back_btn" onclick="history.back()">
@@ -12,7 +12,8 @@
                 <div class="input_row mt-24">
                     <label for="" class="form_label">Old Password </label>
                     <div class="password_area">
-                        <input type="password" placeholder="Enter old password" class="input_field" />
+                        <input type="password" wire:model.blur='old_password' placeholder="Enter old password"
+                            class="input_field" />
                         <div class="password_btn_area">
                             <button type="button" class="eye_open passwordVisibilityBtn">
                                 <img src="{{ asset('assets/app/icons/eye.svg') }}" alt="eye icon" />
@@ -22,15 +23,20 @@
                             </button>
                         </div>
                     </div>
-                    <div class="form_status error">
-                        <img src="{{ asset('assets/app/icons/info-circle.svg') }}" alt="info icon" class="info_circle_red" />
-                        You can’t save the new password with the previous password
-                    </div>
+                    @error('old_password')
+                        <div class="form_status error">
+                            <img src="{{ asset('assets/app/icons/info-circle.svg') }}" alt="info icon"
+                                class="info_circle_red" />
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
+
                 <div class="input_row">
                     <label for="" class="form_label">New Password </label>
                     <div class="password_area">
-                        <input type="password" placeholder="Enter new password" class="input_field" />
+                        <input type="password" wire:model.blur='password' placeholder="Enter new password"
+                            class="input_field" />
                         <div class="password_btn_area">
                             <button type="button" class="eye_open passwordVisibilityBtn">
                                 <img src="{{ asset('assets/app/icons/eye.svg') }}" alt="eye icon" />
@@ -40,12 +46,21 @@
                             </button>
                         </div>
                     </div>
-                    <div class="form_status success">Password is strong</div>
+
+                    @error('password')
+                        <div class="form_status error">
+                            <img src="{{ asset('assets/app/icons/info-circle.svg') }}" alt="info icon"
+                                class="info_circle_red" />
+                            {{ $message }}
+                        </div>
+                    @enderror
+
                 </div>
                 <div class="input_row">
                     <label for="" class="form_label">Confirm Password </label>
                     <div class="password_area">
-                        <input type="password" placeholder="Enter your confirm password" class="input_field" />
+                        <input type="password" wire:model.blur='confirm_password'
+                            placeholder="Enter your confirm password" class="input_field" />
                         <div class="password_btn_area">
                             <button type="button" class="eye_open passwordVisibilityBtn">
                                 <img src="{{ asset('assets/app/icons/eye.svg') }}" alt="eye icon" />
@@ -55,14 +70,17 @@
                             </button>
                         </div>
                     </div>
-                    <div class="form_status error">
-                        <img src="{{ asset('assets/app/icons/info-circle.svg') }}" alt="info icon" class="info_circle_red" />
-                        Password is weak
-                    </div>
+                    @error('confirm_password')
+                        <div class="form_status error">
+                            <img src="{{ asset('assets/app/icons/info-circle.svg') }}" alt="info icon"
+                                class="info_circle_red" />
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <div class="w-100">
-                <button type="submit" class="login_btn">Save</button>
+                <button type="submit" class="login_btn">{!! loadingStateWithText('updatePassword', 'Save') !!}</button>
             </div>
         </form>
     </section>
