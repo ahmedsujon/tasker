27ed8ece06c94a4ed084e986909e23833368efc7
ahmedsujon <sujonahmed424@gmail.com>
2024-11-21 19:58:01 +0600

    terms condition page

---
+++ app/Livewire/App/Profile/Pages/PrivacyPolicyComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Profile\Pages;
+
+use Livewire\Component;
+
+class PrivacyPolicyComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.profile.pages.privacy-policy-component')->layout('livewire.app.layouts.base');
+    }
+}
